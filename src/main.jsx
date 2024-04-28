12f2da331dda62a867a86bda25232026a5cc7cb0
Glauton Osório <glautonleao@gmail.com>
2024-04-28 04:10:02 -0300

    feat: esqueleto de rotas criado, adicionando AuthProvidader no Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { GlobalStyle } from "./Global.style.jsx";
 import { ThemeProvider } from "./contexts/Theme.context.jsx";
+import { RouterProvider } from "react-router-dom";
+import { AppRouter } from "./routes/AppRoutes.jsx";
+import { AuthContextProvider } from "./contexts/Auth.context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider>
-    <App />
-    <GlobalStyle />
+    <AuthContextProvider>
+      <RouterProvider router={AppRouter} />
+      <GlobalStyle />
+    </AuthContextProvider>
   </ThemeProvider>
 );
---
+++ src/routes/PrivateRoutes.jsx
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/Auth.context";
+import { Navigate } from "react-router-dom";
+
+export const PrivateRoutes = ({ children }) => {
+  const { isLogged } = useContext(AuthContext);
+  return isLogged ? children : <Navigate to={"login"} />;
+};
+
+PrivateRoutes.propTypes = {
+  children: PropTypes.node.isRequired,
+};
