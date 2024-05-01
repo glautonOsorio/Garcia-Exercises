1e89df6f99f6f62f343d52aad54bbb8b7340f70a
Glauton Osório <glautonleao@gmail.com>
2024-05-01 17:35:56 -0300

    feat: component de header criado + styles

---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,77 @@
+import { useContext, useEffect } from "react";
+import * as Styled from "./Header.styles";
+import { LoginContext } from "../../contexts/Login.context";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/Auth.context.jsx";
+import { ThemeContext } from "../../contexts/Theme.context.jsx";
+import LoginIcon from "@mui/icons-material/Login";
+import HikingIcon from "@mui/icons-material/Hiking";
+import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
+import ContrastIcon from "@mui/icons-material/Contrast";
+
+export function HeaderComponent() {
+  const { showRegister } = useContext(LoginContext);
+  const { user } = useContext(AuthContext);
+
+  const { theme, changeTheme } = useContext(ThemeContext);
+  const navigate = useNavigate();
+
+  const toRegister = () => {
+    showRegister();
+    navigate("/login");
+  };
+  useEffect(() => {
+    console.log(user);
+  }, []);
+
+  return (
+    <Styled.HeaderContainer $theme={theme}>
+      <Styled.HeaderTextContent>
+        <Styled.HeaderTitle
+          onClick={() => {
+            navigate("/");
+          }}
+          $theme={theme}
+        >
+          Garcia Exercises <HikingIcon fontSize="large" />
+        </Styled.HeaderTitle>
+      </Styled.HeaderTextContent>
+
+      <Styled.HeaderTextContent>
+        <Styled.HeaderColumm onClick={changeTheme} $theme={theme}>
+          <ContrastIcon />
+          <Styled.HeaderSpan $theme={theme}>Mudar tema</Styled.HeaderSpan>
+        </Styled.HeaderColumm>
+
+        {user ? (
+          <>
+            {user.gender === "Male" ? (
+              <Styled.HeaderColumm>
+                <Styled.LoggedIconMan $theme={theme} />
+                <Styled.HeaderSpan $theme={theme}>
+                  {user.name}
+                </Styled.HeaderSpan>
+              </Styled.HeaderColumm>
+            ) : (
+              <Styled.HeaderColumm>
+                <Styled.LoggedIconWoman $theme={theme} />
+                <Styled.HeaderSpan $theme={theme}>
+                  {user.name}
+                </Styled.HeaderSpan>
+              </Styled.HeaderColumm>
+            )}
+          </>
+        ) : (
+          <>
+            <Styled.StyledLink to="/login" $theme={theme}>
+              Fazer Login <LoginIcon fontSize="large" />
+            </Styled.StyledLink>
+            <Styled.StyledLink $theme={theme} onClick={toRegister}>
+              Cadastrar <AppRegistrationIcon fontSize="large" />
+            </Styled.StyledLink>
+          </>
+        )}
+      </Styled.HeaderTextContent>
+    </Styled.HeaderContainer>
+  );
+}
